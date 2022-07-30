70d6f53229b537424f798231fddf707b20651708
Mohamed ELtanani <meltanan@uncc.edu>
2022-07-30 02:03:44 -0400

    added fragments

--- app/src/main/java/com/example/cryptos/Extentions.kt
+++ app/src/main/java/com/example/cryptos/Extentions.kt
@@ -5,10 +5,10 @@ import android.widget.Toast
 import androidx.fragment.app.*
 
 inline fun <reified T : Fragment> FragmentActivity.addFragment(addToBackStack: Boolean = false) {
-//    supportFragmentManager.commit {
-//        if (addToBackStack) addToBackStack(null)
-//        add<T>(R.id.fragmentContainerView)
-//    }
+    supportFragmentManager.commit {
+        if (addToBackStack) addToBackStack(null)
+        add<T>(R.id.fragmentContainerView)
+    }
 }
 
 fun Context.showToast(message: String, length: Int = Toast.LENGTH_SHORT) {
--- app/src/main/java/com/example/cryptos/ui/MainActivity.kt
+++ app/src/main/java/com/example/cryptos/ui/MainActivity.kt
@@ -3,14 +3,19 @@ package com.example.cryptos.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import androidx.activity.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.cryptos.R
+import com.example.cryptos.addFragment
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private val viewModel: HomeActivityViewModel by viewModels()
+    lateinit var bitCointButton: Button
+    lateinit var allCryptos: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,14 +26,20 @@ class MainActivity : AppCompatActivity() {
 
             Log.d("demo11", wow?.size.toString())
 
-
-            //   Log.d("demo", wow.toString())
         }
 
         viewModel.bitCoin.observe(this) {
             Log.d("demo", it.toString())
         }
 
+        setUpUi()
+    }
 
+    private fun setUpUi() {
+        bitCointButton = findViewById(R.id.bitcoint_button)
+        allCryptos = findViewById(R.id.all_cryptos_button)
+
+        bitCointButton.setOnClickListener(){ addFragment<BitCoinFragment>() }
+        allCryptos.setOnClickListener(){ addFragment<AllCryptosFragment>() }
     }
 }
