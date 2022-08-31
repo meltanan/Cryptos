bd0efcc26c419c1f900079b242b8bd64fbe24fb7
Mohamed Eltanani <mohamedeltanani@Mohameds-MacBook-Pro.local>
2022-08-31 13:31:20 -0400

    added module to provide dispatcher provider

--- app/src/main/java/com/example/cryptos/di/AppModule.kt
+++ app/src/main/java/com/example/cryptos/di/AppModule.kt
@@ -2,10 +2,13 @@ package com.example.cryptos.di
 
 import com.example.cryptos.common.Constants
 import com.example.cryptos.network.Remote.CryptosAPI
+import com.example.cryptos.util.DispatcherProvider
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
@@ -20,5 +23,19 @@ object AppModule {
     @Singleton
     fun provideCryptosAPI(): CryptosAPI {
         return Retrofit.Builder().baseUrl(Constants.BASE_URL).addConverterFactory(GsonConverterFactory.create()).build().create()
+    }
+
+        @Provides
+    @Singleton
+    fun provideDispatcher(): DispatcherProvider = object : DispatcherProvider {
+        override val main: CoroutineDispatcher
+            get() = Dispatchers.Main
+        override val io: CoroutineDispatcher
+            get() = Dispatchers.IO
+        override val default: CoroutineDispatcher
+            get() = Dispatchers.Default
+        override val unconfined: CoroutineDispatcher
+            get() = Dispatchers.Unconfined
+
     }
 }
